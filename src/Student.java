436d220e9084cd62fbe2d1d48e1c367490ece39c
PATTABHI VAISHNAVI <pattabhivaishnavi087@gmail.com>
2023-03-29 18:48:27 +0530

    for loop commit

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,57 @@
 public class Student {
+
+    int num;
+    String name;
+
+
+    //CONSTRUCTOR IN JAVA
+    public Student()
+    {
+
+        num=1;
+        name="vaishnavi";
+
+
+    }
     public static void main(String[] args){
-        int i;
+        int i,n;
+
+        Student student= new Student();
+
+
         //FOR LOOP
         for(i=0;i<10;i++)
         {
             System.out.println(i);
+
         }
+
+
+        //While loop
+        while(i<=10)
+        {
+            System.out.println("The value of i is " + i);
+            i--;
+            if(i==0)
+            {
+                break;
+            }
+        }
+
+
+        //Do while loop
+        do {
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+
+
+
+        System.out.println("Name of student is " + student.name +"Roll no is " + student.num );
+
     }
+
+
+
 }
+
