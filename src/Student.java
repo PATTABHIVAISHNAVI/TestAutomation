cd267c651d40aacdd4c33e2a6e03f12a9006432c
PATTABHI VAISHNAVI <pattabhivaishnavi087@gmail.com>
2023-03-29 17:48:31 +0530

    for loop commit

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public static void main(){
+        int i;
+        //FOR LOOP
+        for(i=0;i<10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
